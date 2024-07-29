22d303d9fb91334d45d12cc53d29e36c557e170d
tttjm1 <156225710+tttjm1@users.noreply.github.com>
2024-07-29 21:41:22 +0100

    Update operatingModes.h
    latest driveability refinement.

--- muddersMIMA_firmware/operatingModes.h
+++ muddersMIMA_firmware/operatingModes.h
@@ -4,13 +4,13 @@
 #ifndef modes_h
 	#define modes_h
 	
-	#define  Boost 	35  	// must be > Cruise
-	#define  Cruise 25 	// provides assist during cruise. 
-	#define  Coast 	12  	// compensates for voltage spoofing, must be >10
-	#define  Brake 	37   	// replaces OEM brake regen to mask 3rd gear glitch & provide full regen under braking in all gears
+	#define  Cruise 10 	// default=10 provides assist during cruise. 
+	#define  Boost 	12 	// must be > or = Cruise
+	#define  Coast 	12   	// off-throttle regen, level reduced to offset increase in power with increased voltage
+	#define  Brake 	15   	// replicates OEM brake regen buts masks 3rd gear glitch to provide full regen under braking in all gears
 	#define	 tpsoffset 9 	// throttle position offset at zero
-	#define	 maxmph 112	// maximum MPH safeguard
-				// LIBCM config (REDUCE_BACKGROUND_REGEN_UNLESS_BRAKING) does not need to be defined since this mode does not allow background regen.
+	#define	 maxmph 112 	// maximum MPH safeguard
+				 // LIBCM config (REDUCE_BACKGROUND_REGEN_UNLESS_BRAKING) does not need to be defined since this mode does not allow background regen.
 	void operatingModes_handler(void);
 
 #endif
